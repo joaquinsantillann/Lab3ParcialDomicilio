a2061306b5127e1d001b06cce165f73f1843391b
joaquinsantillann <williamsjoaquinsantillan@gmail.com>
2025-05-20 19:48:37 -0300

    Add files via upload

--- src/ecuacuacionLineal/Termino.java
+++ src/ecuacuacionLineal/Termino.java
@@ -1,43 +1,72 @@
-
 package ecuacuacionLineal;
 
 import java.text.DecimalFormat;
+import utilidades.Consola;
 
-public class Termino <T extends Number> {
-    private T cooeficiente;
+public class Termino<T extends Number> {
+
+    private T coeficiente;
     private char variable;
 
-    public Termino(T cooeficiente, char variable) {
-        this.cooeficiente = cooeficiente;
-        this.variable = variable;
+    public Termino() {
+        this.coeficiente = null;
+        this.variable = ' ';
     }
 
+    public void generarTerminoInt (char x){
+        leerTerminoInt();
+        setVariable(x);
+    }
+    
+    public void generarTerminoDouble ( char x){
+        leerTerminoDouble();
+        setVariable(x);
+    }
+    
+    private void leerTerminoInt() {
+        Integer x;
+        do {
+            Consola.emitirMensaje("Ingresar coeficiene entero:");
+             x = Consola.leerInt();
+            if (x == 0) {
+                Consola.emitirMensajeLN("Error, el coeficiente no debe ser igual a 0");
+            }
+        } while (x == 0);
+        setCooeficiente((T)x);
+    }
+
+    private void leerTerminoDouble() {
+        Double x;
+        do {
+            Consola.emitirMensaje("Ingresar coeficiene real:");
+             x = Consola.leerDouble();
+            if (x == 0) {
+                Consola.emitirMensajeLN("Error, el coeficiente no debe ser igual a 0");
+            }
+        } while (x == 0);
+        setCooeficiente((T)x);
+    }
+    
     public T getCooeficiente() {
-        return cooeficiente;
+        return coeficiente;
     }
 
-    public void setCooeficiente(T cooeficiente) {
-        this.cooeficiente = cooeficiente;
+    private void setCooeficiente(T cooeficiente) {
+        this.coeficiente = cooeficiente;
     }
 
     public char getVariable() {
         return variable;
     }
 
-    public void setVariable(char variable) {
+    private void setVariable(char variable) {
         this.variable = variable;
     }
 
     @Override
     public String toString() {
         DecimalFormat formato = new DecimalFormat("0.##");
-        return "" + formato.format(this.cooeficiente ) + this.variable + "";
+        return "" + formato.format(this.coeficiente) + this.variable + "";
     }
 
-    
-    
-    
-    
-    
-    
 }
